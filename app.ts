fc814873ae59bdc85f4efb406ced6d51cc85eaf2
anjalikesherwani <122610979+anjalikesherwani@users.noreply.github.com>
2024-02-13 16:06:21 +0530

    first commit

---
+++ app.ts
@@ -0,0 +1,35 @@
+//for string
+let Name: string = "Anjali";
+console.log("Name is : ",Name)
+
+//for number
+let a:number=20;
+let b:number=10;
+const sum:number=a+b;
+console.log("Sum is : ",sum);
+
+//for string
+let str:string="Anjali";
+const cnst: string = str + " How are you?"
+console.log("string is : ",cnst);
+
+
+//for boolean
+let variable1:boolean=true;
+const isTrue:boolean = variable1 && true;
+console.log("Boolean is : ",isTrue)
+
+
+//for indexing
+enum  Color {Red, Green, Blue, yellow};
+console.log("Color is : ", Color)
+let myColor:Color=Color.Green;
+console.log("Color is :",myColor);
+
+
+//For null and undefined
+let abc:null=null;
+let xyz:undefined=undefined;
+console.log("abc is : ",abc,"\n","xyz is : ",xyz);
+
+
